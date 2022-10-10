cae04fd6231f308a38ad43c9a9aa5a0bcfc2e96c
Ansen123 <ansenfp@gmail.com>
2022-10-10 14:23:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ShopingCartListComponent } from './shoping-cart-list/shoping-cart-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ShopingCartListComponent
   ],
   imports: [
     BrowserModule,
