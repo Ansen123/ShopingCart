4201d4203bbd9568355865d3c96e114a6ce8a53a
Ansen123 <ansenfp@gmail.com>
2022-10-10 14:25:59 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,11 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ShopingCartListComponent } from './shoping-cart-list/shoping-cart-list.component';
 
-
+const appRoutes:Routes=[
+  {
+    path:"",component:ShopingCartListComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
